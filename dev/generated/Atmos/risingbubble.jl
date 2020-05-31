4791ee635306f7da9227494a4661a39720c0d67d
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2020-05-31 22:47:04 +0000

    build based on d59fc1f

--- dev/generated/Atmos/risingbubble.jl
+++ dev/generated/Atmos/risingbubble.jl
@@ -15,7 +15,7 @@ using ClimateMachine.Mesh.Filters
 
 using ClimateMachine.TemperatureProfiles
 
-using ClimateMachine.MoistThermodynamics
+using ClimateMachine.Thermodynamics
 
 using ClimateMachine.VariableTemplates
 
